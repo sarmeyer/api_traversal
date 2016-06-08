f98e30864cdb9e2ca00e2a4f3b8f334aa636993e
Sarah <sarahmichellemeyer@gmail.com>
2016-06-08 10:49:36 -0600

    complete movie search warmup

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
     var input = $('#userInput').val();
     $.get('http://www.omdbapi.com/?s=' + input, function(data, status){
       var movieArr = data.Search;
-      var alternate = '/mockups/images/no_image.png';
-      console.log(alternate);
+      var alternate = './mockups/images/no_image.png'
       movieArr.forEach(function(movie){
-        $('#center_column').append('<p>' + movie.Title + '</p>' + '<img class="movie_poster" src=' + movie.Poster + '>');
-        // $('.movie_poster').each(function(){
-        //     $(this).attr('alt', 'src=mockups/images/no_image.png');
-        // });
+        console.log(movie.Poster);
+        if (movie.Poster === 'N/A') {
+            $('#center_column').append('<p>' + movie.Title + '</p><img class="movie_poster" src=' + alternate + '>');
+        } else {
+        $('#center_column').append('<p>' + movie.Title + '</p><img class="movie_poster" src=' + movie.Poster + '>');
+        }
       })
     })
   })
