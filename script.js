6cd4fde2b97c2f04910122001f4de3687819abdd
Sarah <sarahmichellemeyer@gmail.com>
2016-06-08 10:36:49 -0600

    alignment adjusted, need to adjust alt images for each image tag

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
     var input = $('#userInput').val();
     $.get('http://www.omdbapi.com/?s=' + input, function(data, status){
       var movieArr = data.Search;
+      var alternate = '/mockups/images/no_image.png';
+      console.log(alternate);
       movieArr.forEach(function(movie){
-        $('#center_column').append('<p>' + movie.Title + '<img class="movie_poster" src=' + movie.Poster + '></p>');
+        $('#center_column').append('<p>' + movie.Title + '</p>' + '<img class="movie_poster" src=' + movie.Poster + '>');
+        // $('.movie_poster').each(function(){
+        //     $(this).attr('alt', 'src=mockups/images/no_image.png');
+        // });
       })
-      console.log(movieArr);
     })
   })
 })
